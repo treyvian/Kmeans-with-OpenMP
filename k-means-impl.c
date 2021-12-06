5e0eb76550a604fc69e974eb02e8e796e02630c6
Treyvian <davide.pozzoli@gmail.com>
2021-12-06 19:07:55 +0100

    Dealing with multiple dimensions, still no output, but core-segmentation resolved

--- k-means-impl.c
+++ k-means-impl.c
@@ -5,33 +5,49 @@
 
 
 struct point {
-    double x, y;
+    double *x;
+    int dimensions;
     int cluster;
     double minDist;
 };
 
 typedef struct point point;
 
-point *point_in(double x, double y){
+point *point_in (double *x, int dimensions) {
+    
     point *p = (point *)malloc(sizeof(point));
     p->x = x;
-    p->y = y;
+    p->dimensions = dimensions;
     p->cluster = -1;
     p->minDist = __DBL_MAX__;
+
     return p;
 }
 
-double distance(point p1, point p2){
-    return (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y);
+double distance (point *p1, point *p2) {
+    double dist;
+    
+    for (int i=0; i< p1->dimensions; i++) {
+        dist += (p2->x[i] - p1->x[i]) * (p2->x[i] - p1->x[i]);
+    }
+    
+    return dist;
 }
 
-void print(point *p){
-    printf("(%f,%f)\n", p->x, p->y);
+void print_point (point *p) {
+    printf("(");
+    
+    for (int i=0; i < p->dimensions; i++) {
+        printf("%f ", p->x[i]);
+    }
+
+    printf("]\n");
 }
 
-void read_csv(int row, int col, char *filename, int **data){
-	FILE *file;
-	file = fopen(filename, "r");
+void read_csv (int row, int col, char *filename, int **data) {
+	
+    FILE *file;
+	file = fopen (filename, "r") ;
 
 	int i = 0;
     char line[4098];
@@ -53,12 +69,17 @@ void read_csv(int row, int col, char *filename, int **data){
 }
 
 
-void kMeansClustering(point *points, const int n, const int epochs, const int k){
+void kMeansClustering (point *points, 
+                        const int n, 
+                        const int epochs, 
+                        const int k) {
+
     point *centroids = (point *)malloc(k * sizeof(point));
     srand(time(0));
     for (int i = 0; i < k; ++i) {
         centroids[i] = points[(rand() % n)];
     }
+
 
     for (int t = 0; t<epochs; t++) {
         
@@ -66,7 +87,7 @@ void kMeansClustering(point *points, const int n, const int epochs, const int k)
             int cluster_id = i;
 
             for (int j=0; j<n; j++) {
-                double dist = distance(centroids[i], points[j]);
+                double dist = distance(&centroids[i], &points[j]);
                 if (dist < points[j].minDist){
                     points[j].minDist = dist;
                     points[j].cluster = cluster_id;
@@ -74,36 +95,39 @@ void kMeansClustering(point *points, const int n, const int epochs, const int k)
             }
         }
 
-        //for (int i=0; i<n; i++){
-        //    printf("%f - %d\n",points[i].minDist, points[i].cluster);
-        //}
-
         int *nPoints = (int *)malloc(k * sizeof(int));
-        double *sumX = (double *)malloc(k * sizeof(double)); 
-        double *sumY = (double *)malloc(k * sizeof(double));
+        double **sum = (double **)malloc(k * sizeof(double *));
+        for (int i = 0; i < k; ++i) {
+		    sum[i] = (double *)malloc(points[0].dimensions * sizeof(double));
+	    }
 
         // Initialise with zeroes
         for (int j = 0; j < k; ++j) {
             nPoints[j] = 0;
-            sumX[j] = 0.0;
-            sumY[j] = 0.0;
+            for (int i = 0; i< points[0].dimensions; i++) {
+                sum[j][i] = 0.0;
+            }    
         }
 
         // Iterate over points to append data to centroids
         for (int i=0; i<n; i++) {
             int cluster_id = points[i].cluster;
             nPoints[cluster_id] += 1;
-            sumX[cluster_id] += points[i].x; 
-            sumY[cluster_id] += points[i].y;
-        
+            
+            for (int j = 0; j < points[i].dimensions; ++j) {
+                sum[cluster_id][j] += points[i].x[j]; 
+            }
+
             points[i].minDist = __DBL_MAX__;
         }
 
         // Compute new centroids
         for (int i=0; i<k; ++i) {
             int cluster_id = i;
-            centroids[i].x = sumX[cluster_id]/nPoints[cluster_id];
-            centroids[i].y = sumY[cluster_id]/nPoints[cluster_id];
+
+            for (int j = 0; j<centroids->dimensions; j++) {
+                centroids[i].x[j] = sum[cluster_id][j]/nPoints[cluster_id];
+            }
         }
     }
 }
@@ -119,12 +143,7 @@ void create_marks_csv(char *filename, point *points,int n){
 
     fp=fopen(filename,"w+");
 
-    fprintf(fp,"X,Y,Cluster\n");
-    
-    for (int i=0;i<n;i++) {
-    
-        fprintf(fp,"%f,%f,%d\n", points[i].x, points[i].y, points[i].cluster);
-    }    
+    fprintf(fp,"X,Y,Cluster\n");  
     
     fclose(fp);
     
@@ -155,8 +174,11 @@ int main (int argc, char const *argv[]) {
     point *data;
     data = (point *)malloc((row) * sizeof(point));
 
-    for (int i = 0; i < row; i++) {
-        data[i] = *point_in(dat[i][3], dat[i][4]);
+    double *p = (double *)malloc(2 * sizeof(double));
+    for (int i = 0; i < row; i++) { 
+        p[0] = (double) dat[i][3];
+        p[1] = (double) dat[i][4];
+        data[i] = *point_in(p, 2);
     }
 
     const size_t data_size = row;
@@ -167,10 +189,8 @@ int main (int argc, char const *argv[]) {
     }
     free(dat);
 
-
+    
     kMeansClustering(data, data_size, 100, 5);
-
-    printf("prova");
 
     char *name = "output.csv";
 
