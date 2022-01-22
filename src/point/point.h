1d7079514bd6ba85eb4d903985e9e49624df81c9
treyvian <davide.pozzoli6@gmail.com>
2022-01-23 00:46:48 +0100

    removed single files for each methods

--- src/point/point.h
+++ src/point/point.h
@@ -14,41 +14,66 @@ typedef struct point {
 
 /*
 * Initiate a point with the parameters in input
+* 
+* @param p the point which will be initialized
+* @param x the coordinates of the point
+* @param dimensions dimensions of the point
 */
-void point_init (point*, double*, int);
+void point_init (point *p, double *x, int dimensions);
 
 /*
 * Calculate the euclidian distance between 2 points
+*
+* @param p1 first point to take in consideration
+* @param p2 second point to take in consideration
+* @return the distance between p1 and p2
 */
-double euclidian_distance (point *, point *);
+double euclidian_distance (point *p1, point *p2);
 
 /*
 * Calculate the manhattan distance between 2 points
+*
+* @param p1 first point to take in consideration
+* @param p2 second point to take in consideration
+* @return the distance between p1 and p2
 */
-double manhattan_distance (point *, point *);
+double manhattan_distance (point *p1, point *p2);
 
 /*
 * Copy a point to another
+*
+* @param dst destination of the copy process
+* @param src source point that needs to be copied
 */
-void copy_point (point *, const point *);
+void copy_point (point *dst, const point *src);
 
 /*
 * Reset the struct point to the initial values
+*
+* @param point that needs to be resetted to inital values
 */
-void reset_point (point *);
+void reset_point (point *p);
 
 /*
 * Check if two points have the same coordinates
+*
+* @param p1 first point to compare
+* @param p2 second point to compare
+* @return 1 if they have the same coordinates 0 otherwise
 */
-int equals (const point *, const point *);
+int equals (const point *p1, const point *p2);
 
 /*
 * Print the point coordinates
+*
+* @param p point that needs to be printed
 */
-void print_point (const point *);
+void print_point (const point *p);
 
 /*
 * Free the coordinates array inside the struct
+*
+* @param free the coordinate array x
 */
 void delete_point(point *);
 
--- src/silhouette-score/silhouette.h
+++
@@ -1,9 +0,0 @@
-#ifndef SILHOUETTE_H
-
-#define SILHOUETTE_H
-
-#include "../point/point.h"
-
-double silhouette_score (point *, int, int);
-
-#endif
